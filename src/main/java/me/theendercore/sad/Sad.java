b4354563f8d5a075d4d261a502d609e0d125c61c
theendercore <theendercore@gmail.com>
2022-08-15 22:03:40 +0300

    config

--- src/main/java/me/theendercore/sad/Sad.java
+++ src/main/java/me/theendercore/sad/Sad.java
@@ -1,17 +1,26 @@
 package me.theendercore.sad;
 
 import me.theendercore.sad.commands.SadCommand;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Item;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.logging.Logger;
+import java.util.HashMap;
+import java.util.Map;
 
 public final class Sad extends JavaPlugin {
 
-    public Logger LOGGER = getLogger();
+    public static Map<String, Item> kits = new HashMap<>();
+
     @Override
     public void onEnable() {
-        LOGGER.info("S.A.D. Has been init()");
+        this.getConfig().addDefault("pp", 5);
+        this.saveDefaultConfig();
+
+
         getCommand("sad").setExecutor(new SadCommand());
+
+        getLogger().info("S.A.D. Has been ENABLED");
     }
 
     @Override
--- src/main/java/me/theendercore/sad/commands/SadCommand.java
+++ src/main/java/me/theendercore/sad/commands/SadCommand.java
@@ -1,16 +1,32 @@
 package me.theendercore.sad.commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
 
 public class SadCommand implements CommandExecutor {
 
+
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        sender.sendMessage("It ran");
+        if(!(sender instanceof Player)) {
+            sender.sendMessage("Only players can run this command !");
+            return false;
+        }
+        Player p = (Player) sender;
+        if (!p.hasPermission("sad.kit.command")){
+            p.sendMessage(ChatColor.RED + "You don't have the perms");
+            return false;
+        }
 
+        p.sendMessage("yay u have perms");
 
-        return false;
+        return true;
     }
 }
